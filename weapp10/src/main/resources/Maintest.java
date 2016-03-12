054a081b09d8f9acce0d48d815e5968458ef9eb6
jhx9 <jimmy.jhhuang@mic.com.tw>
2016-03-12 20:57:40 +0800

    initial commit

---
+++ weapp10/src/main/resources/Maintest.java
@@ -0,0 +1,13 @@
+import statis org.junit.Assert.*;
+
+public class Maintest {
+
+//	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	@Test
+		\public voide test()
+			fail{"I am done");
+	}
+
+}
